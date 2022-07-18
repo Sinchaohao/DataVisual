6fc316d6aefa2c05f5a94e6b0125e7f01a304089
Sch <Sinchaohao@outlook.com>
2022-07-18 17:16:27 +0800

    auto_save picture

--- scatter_squares.py
+++ scatter_squares.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [ x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolors='none', s=10)
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolors='none', s=10)
 
 # 设置图表标题并给坐标轴加上标签
 plt.title("Square Numbers", fontsize=24)
@@ -20,4 +20,6 @@ plt.ylabel("Square of Value", fontsize=14)
 # plt.tick_params(axis='both', which='major', labelsize=14)
 
 plt.axis([0, 1100, 0, 1100000])
-plt.show()
+# plt.show()
+# bbox_inches:指定将图表多余的空白区域裁剪掉。如果要保留图表周围多余的空 白区域，可省略这个实参
+plt.savefig('squares_plot.png', bbox_inches='tight')
