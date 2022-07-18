c86012f7aed16d5e1e1cb1813db4fc9b8509e50f
Sch <Sinchaohao@outlook.com>
2022-07-18 16:32:40 +0800

    Initial commit

---
+++ mpl_squares.py
@@ -0,0 +1,11 @@
+# @Author:sch
+# @Time:2022/07/18
+# @File:mpl_squares.py
+# @software: PyCharm
+# description: 绘制简单的折线图
+
+import matplotlib.pyplot as plt
+
+squares = [1,4,9,16,25]
+plt.plot(squares)
+plt.show()
