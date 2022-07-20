2af6da8d19243ab593bed2b8637b22868a8cbb2e
Sch <Sinchaohao@outlook.com>
2022-07-20 17:22:34 +0800

    AKTD-July Temperature

--- downloadData/highs_lows.py
+++ downloadData/highs_lows.py
@@ -5,6 +5,8 @@
 
 import csv
 
+import matplotlib.pyplot as plt
+
 filename = 'sitka_weather_07-2014.csv'
 
 # 从文件中获取最高气温
@@ -18,6 +20,19 @@ with open(filename) as f:
 
     highs = []
     for row in reader:
-        highs.append(row[1])
+        high = int(row[1])
+        highs.append(high)
 
     print(highs)
+
+# 根据数据绘制图形
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(highs, c='red')
+
+# 设置图形的格式
+plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major',labelsize=16)
+
+plt.show()
