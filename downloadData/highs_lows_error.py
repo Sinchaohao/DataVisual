6e6d5cb7d34307d51aaab944fa09b92b227b10bc
Sch <Sinchaohao@outlook.com>
2022-07-20 18:56:25 +0800

    处理错误数据

---
+++ downloadData/highs_lows_error.py
@@ -0,0 +1,50 @@
+# @Author:sch
+# @Time:2022/07/20
+# @File:highs_lows_error.py
+# @software: PyCharm
+
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+# filename = 'sitka_weather_07-2014.csv'
+filename = 'death_valley_2014.csv'
+# 从文件中获取日期与最高气温、最低气温
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # for index, column_header in enumerate(header_row):
+    #     # enumerate() 来获取每个元素的索引及其值
+    #     print(index, column_header)
+
+    dates, highs, lows = [], [], []
+    for row in reader:
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+
+    print(highs)
+
+# 根据数据绘制图形
+fig = plt.figure(dpi=128, figsize=(10, 6))
+# alpha 指定颜色的透明度。Alpha 值为0表示完全透明，1（默认设置）表示完全不透明
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='green', alpha=0.1)
+
+# 设置图形的格式
+plt.title("Daily high&low temperatures - 2014\nDeath Valley,CA", fontsize=20)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major',labelsize=16)
+
+plt.show()
