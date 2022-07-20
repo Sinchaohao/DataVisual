b21ac0e9ecd0ab922a2fadcb869ad1e56fc8843c
Sch <Sinchaohao@outlook.com>
2022-07-20 21:35:16 +0800

    读取json数据绘制WorldMap

--- jsonData/world_population.py
+++ jsonData/world_population.py
@@ -3,8 +3,10 @@
 # @File:world_population.py
 # @software: PyCharm
 
-import json
+# description：绘制完整的世界人口地图
 
+import json
+import pygal_maps_world.maps
 from country_codes import get_country_code
 
 # 将数据加载到一个列表中
@@ -13,14 +15,18 @@ with open(filename) as f:
     pop_data = json.load(f)
 
 
-# 打印每个国家2010年的人口数量
+# 创建一个包含人口数量的字典
+cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
         code = get_country_code(country_name)
         if code:
-            print(code + ": " + str(population))
-        else:
-            print(country_name + " code missing")
+            cc_populations[code] = population
 
+wm = pygal_maps_world.maps.World()
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010', cc_populations)
+
+wm.render_to_file('world_population.svg')
