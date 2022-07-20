16b5ea744a17d6e21230e31249c9e5d916748b4b
Sch <Sinchaohao@outlook.com>
2022-07-20 18:50:06 +0800

    两线间隙填充

--- downloadData/highs_lows.py
+++ downloadData/highs_lows.py
@@ -32,8 +32,10 @@ with open(filename) as f:
 
 # 根据数据绘制图形
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red')
-plt.plot(dates, lows, c='blue')
+# alpha 指定颜色的透明度。Alpha 值为0表示完全透明，1（默认设置）表示完全不透明
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='green', alpha=0.1)
 
 # 设置图形的格式
 plt.title("Daily high temperatures - 2014", fontsize=24)
